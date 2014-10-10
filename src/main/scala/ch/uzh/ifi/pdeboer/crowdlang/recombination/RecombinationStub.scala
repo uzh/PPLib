6e1bbeb394a53c906355466c80cd0afa498ef123
Patrick de Boer <pdeboer@mit.edu>
2014-10-10 12:25:12 +0200

    added simple recombination stub without pre-/postprocessing

--- src/main/scala/ch/uzh/ifi/pdeboer/crowdlang/recombination/RecombinationStub.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/crowdlang/recombination/RecombinationStub.scala
@@ -4,10 +4,11 @@ package ch.uzh.ifi.pdeboer.crowdlang.recombination
  * Created by pdeboer on 09/10/14.
  */
 class RecombinationStub[INPUT, PROCESS_INPUT, PROCESS_OUTPUT, OUTPUT](
-                                                                       inputPreprocessor: INPUT => PROCESS_INPUT, processor: PROCESS_INPUT => PROCESS_OUTPUT,
-                                                                       outputPostprocessor: PROCESS_OUTPUT => OUTPUT) {
-
-  def run(data: INPUT): OUTPUT =
-    outputPostprocessor(processor(inputPreprocessor(data)))
+																		 inputPreprocessor: INPUT => PROCESS_INPUT, processor: PROCESS_INPUT => PROCESS_OUTPUT,
+																		 outputPostprocessor: PROCESS_OUTPUT => OUTPUT) {
+	def run(data: INPUT): OUTPUT =
+		outputPostprocessor(processor(inputPreprocessor(data)))
 }
 
+class SimpleRecombinationStub[PROCESS_INPUT, PROCESS_OUTPUT](processor: PROCESS_INPUT => PROCESS_OUTPUT) extends RecombinationStub[PROCESS_INPUT, PROCESS_INPUT,
+	PROCESS_OUTPUT, PROCESS_OUTPUT](i => i, processor, o => o)
