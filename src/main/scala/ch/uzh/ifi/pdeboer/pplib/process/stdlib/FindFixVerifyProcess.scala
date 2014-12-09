436cc1fe8d51744c2f22881f2dacdb48a0794fde
Patrick de Boer <pdeboer@mit.edu>
2014-12-09 20:18:45 +0100

    ugly compilation error

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/FindFixVerifyProcess.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/FindFixVerifyProcess.scala
@@ -24,7 +24,7 @@ class FindFixVerifyProcess(params: Map[String, Any] = Map.empty[String, Any]) ex
 		val exec = memoizer.memWithReinitialization("ffvexec")(new FindFixVerifyExecutor(
 			driver, PATCHES_COUNT_IN_FIND.get, FINDERS_COUNT.get,
 			MIN_FINDERS_TO_AGREE_FOR_FIX.get, FIXERS_PER_PATCH.get,
-			memoizer = memoizer
+			_memoizer = memoizer
 		))(exec => {
 			exec.driver = driver
 			exec.memoizer = memoizer
