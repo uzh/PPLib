a231a11f7294f63a84a14c3a46ee20b3f549f414
Patrick de Boer <pdeboer@mit.edu>
2014-12-22 12:37:19 +0100

    findfixpatch process

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/FindFixPatchProcess.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/FindFixPatchProcess.scala
@@ -1,23 +1,27 @@
 package ch.uzh.ifi.pdeboer.pplib.process.stdlib
 
 import ch.uzh.ifi.pdeboer.pplib.process._
-import ch.uzh.ifi.pdeboer.pplib.process.entities.{PassableProcessParam, Patch}
+import ch.uzh.ifi.pdeboer.pplib.process.entities.{IndexedPatch, PassableProcessParam, Patch}
 
 /**
  * Created by pdeboer on 22/12/14.
  */
-class FindFixPatchProcess(_params: Map[String, Any] = Map.empty) extends ProcessStub[List[Patch], List[Patch]](_params) {
+class FindFixPatchProcess(_params: Map[String, Any] = Map.empty) extends ProcessStub[List[IndexedPatch], List[IndexedPatch]](_params) {
 
 	import ch.uzh.ifi.pdeboer.pplib.process.stdlib.FindFixPatchProcess._
 
-	override protected def run(data: List[Patch]): List[Patch] = {
+	override protected def run(data: List[IndexedPatch]): List[IndexedPatch] = {
 		val memoizer: ProcessMemoizer = getProcessMemoizer(data.hashCode() + "").getOrElse(new NoProcessMemoizer())
 
 		val find = FIND_PROCESS.get.create(higherPrioParams = Map(ProcessStub.MEMOIZER_NAME.key -> getMemoizerForProcess(FIND_PROCESS.get, "finder")))
 		val fixer = FIX_PROCESS.get.create(Map(FixPatchProcess.ALL_DATA.key -> data), Map(ProcessStub.MEMOIZER_NAME.key -> getMemoizerForProcess(FIND_PROCESS.get, "fixer")))
 
 		val found = memoizer.mem("find")(find.process(data))
-		memoizer.mem("fix")(fixer.process(found))
+		val fixed: Map[Int, IndexedPatch] = memoizer.mem("fix")(fixer.process(found))
+			.asInstanceOf[List[IndexedPatch]]
+			.map(f => (f.index, f)).toMap
+
+		data.map(d => fixed.get(d.index).getOrElse(d))
 	}
 
 	def getMemoizerForProcess(process: PassableProcessParam[List[Patch], List[Patch]], suffix: String = "") = {
