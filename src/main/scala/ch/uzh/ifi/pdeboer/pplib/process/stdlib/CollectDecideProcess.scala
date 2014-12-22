1bfcc45272edbb4f5bd02e7c1ead1db2a2cdf5ce
Patrick de Boer <pdeboer@mit.edu>
2014-12-22 15:11:57 +0100

    option to not forward params

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/CollectDecideProcess.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/CollectDecideProcess.scala
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.pdeboer.pplib.process.stdlib
 
-import ch.uzh.ifi.pdeboer.pplib.process.entities.{Patch, PassableProcessParam}
 import ch.uzh.ifi.pdeboer.pplib.process._
+import ch.uzh.ifi.pdeboer.pplib.process.entities.{PassableProcessParam, Patch}
 
 /**
  * Created by pdeboer on 05/12/14.
@@ -20,16 +20,20 @@ class CollectDecideProcess(_params: Map[String, Any] = Map.empty) extends Proces
 		val memoizer: ProcessMemoizer = getProcessMemoizer(data.hashCode() + "").getOrElse(new NoProcessMemoizer())
 
 		logger.info("Running collect-phase for patch")
-		val collection: List[Patch] = memoizer.mem("collectProcess")(COLLECT.get.create(params).process(data))
+		val collection: List[Patch] = memoizer.mem("collectProcess")(COLLECT.get.create(if (FORWARD_PARAMS_TO_COLLECT.get) params else Map.empty).process(data))
 		val collectionDistinct = collection.distinct
 		logger.info(s"got ${collection.length} results. ${collectionDistinct.length} after pruning. Running decide")
-		val res = memoizer.mem(getClass.getSimpleName + "decideProcess")(DECIDE.get.create(params).process(collectionDistinct))
+		val res = memoizer.mem(getClass.getSimpleName + "decideProcess")(DECIDE.get.create(if (FORWARD_PARAMS_TO_DECIDE.get) params else Map.empty).process(collectionDistinct))
 		logger.info(s"Collect/decide for $res has finished with Patch $res")
 		res
 	}
+
+	override def optionalParameters: List[ProcessParameter[_]] = List(FORWARD_PARAMS_TO_COLLECT, FORWARD_PARAMS_TO_DECIDE)
 }
 
 object CollectDecideProcess {
+	val FORWARD_PARAMS_TO_COLLECT = new ProcessParameter[Boolean]("forwardParamsToCollect", OtherParam(), Some(List(true)))
+	val FORWARD_PARAMS_TO_DECIDE = new ProcessParameter[Boolean]("forwardParamsToDecide", OtherParam(), Some(List(true)))
 	val COLLECT = new ProcessParameter[PassableProcessParam[Patch, List[Patch]]]("collect", WorkflowParam(), None)
 	val DECIDE = new ProcessParameter[PassableProcessParam[List[Patch], Patch]]("decide", WorkflowParam(), None)
 }
