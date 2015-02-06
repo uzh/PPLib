52011a8c6909f78d45de479674ad4adb84fb4d66
Patrick de Boer <pdeboer@mit.edu>
2015-02-06 14:50:33 +0100

    instruction generation base classes

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/ProcessParameter.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/ProcessParameter.scala
@@ -1,5 +1,7 @@
 package ch.uzh.ifi.pdeboer.pplib.process
 
+import ch.uzh.ifi.pdeboer.pplib.hcomp.{HCompInstructionsWithTupleStringified, HCompInstructionsWithTuple}
+
 import scala.reflect.ClassTag
 
 /**
@@ -15,4 +17,26 @@ import scala.reflect.ClassTag
 	def get(implicit processStub: ProcessStub[_, _]) = processStub.getParam(this)
 
 	override def toString: String = key
+}
+
+class InstructionParameter(candidateDefinitions: Option[Iterable[HCompInstructionsWithTuple]] = None, keyPrefix: String = "") extends ProcessParameter[HCompInstructionsWithTuple](keyPrefix + "question", candidateDefinitions)
+
+trait InstructionGenerator {
+	def generate(base: InstructionData): HCompInstructionsWithTuple
+}
+
+class InstructionData(val actionName: String = "refine the following paragraph",
+					  val detailedDescription: String = "grammar (e.g. tenses), coherence and text-sophistication",
+					  val evaluation: String = "Malicious/unchanged answers will get rejected. Your answer will be evaluated by other crowd workers.") {
+}
+
+class SimpleInstructionGeneratorCreate extends InstructionGenerator {
+	override def generate(base: InstructionData): HCompInstructionsWithTuple = new HCompInstructionsWithTupleStringified(
+		"Please " + base.actionName + "in terms of " + base.detailedDescription, base.evaluation)
+}
+
+class SimpleInstructionGeneratorDecide extends InstructionGenerator {
+	override def generate(base: InstructionData): HCompInstructionsWithTuple = new HCompInstructionsWithTupleStringified(
+		"Before, crowd workers were asked to " + base.actionName, "Please select the one you like best in terms of " + base.detailedDescription
+	)
 }
