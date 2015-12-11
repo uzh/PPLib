f1634abc20e97a6b2017cca7841eb5e805332abb
Patrick de Boer <pdeboer@mit.edu>
2015-12-11 12:13:32 -0600

    function discretization

--- src/test/scala/ch/uzh/ifi/pdeboer/pplib/examples/optimization/MCOptimizationDeepStructure.scala
+++ src/test/scala/ch/uzh/ifi/pdeboer/pplib/examples/optimization/MCOptimizationDeepStructure.scala
@@ -3,7 +3,7 @@ package ch.uzh.ifi.pdeboer.pplib.examples.optimization
 import ch.uzh.ifi.pdeboer.pplib.hcomp.HCompPortalAdapter
 import ch.uzh.ifi.pdeboer.pplib.process.entities._
 import ch.uzh.ifi.pdeboer.pplib.process.recombination._
-import ch.uzh.ifi.pdeboer.pplib.process.stdlib.FixPatchProcess
+import ch.uzh.ifi.pdeboer.pplib.process.stdlib.{ContestWithStatisticalReductionProcess, ContestWithBeatByKVotingProcess, FixPatchProcess}
 
 /**
   * Created by pdeboer on 12/05/15.
@@ -35,6 +35,10 @@ class MCOptimizationDeepStructure extends SimpleDeepStructure[String, MCOptimiza
 		RecombinationSearchSpaceDefinition[DecideProcess[_ <: List[Patch], _ <: Patch]](
 			RecombinationHints.create(Map(
 				RecombinationHints.DEFAULT_HINTS -> {
+					new AddedParameterRecombinationHint[Int](DefaultParameters.MAX_ITERATIONS, 20 to 30) ::
+						new AddedParameterRecombinationHint[Int](DefaultParameters.WORKER_COUNT, 1 to 10) ::
+						new AddedParameterRecombinationHint[Int](ContestWithBeatByKVotingProcess.K, 1 to 10) ::
+						new AddedParameterRecombinationHint[Double](ContestWithStatisticalReductionProcess.CONFIDENCE_PARAMETER, (1 to 8).map(i => 0.6 + (i.toDouble * .05))) ::
 					RecombinationHints.hcompPlatform(List(HCOMP_PORTAL_TO_USE)) :::
 						RecombinationHints.instructions(List(
 							new InstructionData(actionName = "select the element you like most", detailedDescription = "yeah, that one")))
