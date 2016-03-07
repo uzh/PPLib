c05ec724590169f91910fdca2d1cd51eab73a35a
Patrick de Boer <pdeboer@mit.edu>
2016-03-07 10:30:23 +0100

    bayesian truth contest

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/entities/HCompQueryBuilder.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/entities/HCompQueryBuilder.scala
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.pdeboer.pplib.process.entities
 
 import ch.uzh.ifi.pdeboer.pplib.hcomp._
+import ch.uzh.ifi.pdeboer.pplib.process.entities.DefaultParameters._
 
 import scala.reflect.ClassTag
 import scala.util.Random
@@ -10,9 +11,25 @@ import scala.xml.NodeSeq
   * Created by pdeboer on 28/08/15.
   */
 trait HCompQueryBuilder[T] {
-	def buildQuery(input: T, base: ProcessStub[_, _], payload: Any = ""): HCompQuery
+	def buildQuery(input: T, base: ProcessStub[_, _], nonBaseClassInstructionGenerator: Option[InstructionGenerator] = None): HCompQuery
 
 	def parseAnswer[TARGET](input: T, answer: HCompAnswer, base: ProcessStub[_, _])(implicit baseCls: ClassTag[TARGET]): Option[TARGET]
+
+	protected def prepareInstructions(base: ProcessStub[_, _], nonBaseClassInstructionGenerator: Option[InstructionGenerator]): (QuestionRenderer, String) = {
+		if (nonBaseClassInstructionGenerator.isDefined) {
+			val ig: InstructionGenerator = nonBaseClassInstructionGenerator.get
+			val instructionData: InstructionData = base.getParam(INSTRUCTIONS)
+			(ig.generateQuestion(instructionData), ig.generateQuestionTitle(instructionData))
+		} else {
+			base match {
+				case ih: InstructionHandler =>
+					val instructionTitle = ih.instructionTitle + " [" + Math.abs(Random.nextDouble()) + "]"
+
+					(ih.instructions, instructionTitle)
+				case _ => throw new IllegalStateException("We only support Instruction-Handling enabled processes")
+			}
+		}
+	}
 }
 
 import ch.uzh.ifi.pdeboer.pplib.process.entities.DefaultParameters._
@@ -20,17 +37,14 @@ import ch.uzh.ifi.pdeboer.pplib.process.entities.DefaultParameters._
 class DefaultMCQueryBuilder(maxAnswers: Int = 1, minAnswers: Int = 1, italicInstructionsParam: ProcessParameter[String] = INSTRUCTIONS_ITALIC,
 							auxParam: ProcessParameter[Option[NodeSeq]] = QUESTION_AUX, shuffleChoicesParam: ProcessParameter[Boolean] = SHUFFLE_CHOICES) extends HCompQueryBuilder[List[Patch]] {
 
-	override def buildQuery(input: List[Patch], base: ProcessStub[_, _], payload: Any = ""): HCompQuery = {
-		base match {
-			case ih: InstructionHandler =>
-				val instructionItalic: String = base.getParamOption(italicInstructionsParam).getOrElse("")
-				val htmlData: NodeSeq = base.getParamOption(auxParam).getOrElse(Some(Nil)).getOrElse(Nil)
-				val instructions: String = ih.instructions.getInstructions(instructionItalic, htmlData = htmlData)
-				val choices = if (base.getParamOption(shuffleChoicesParam).getOrElse(true)) Random.shuffle(input) else input
+	override def buildQuery(input: List[Patch], base: ProcessStub[_, _], nonBaseClassInstructionGenerator: Option[InstructionGenerator]): HCompQuery = {
+		val instructionItalic: String = base.getParamOption(italicInstructionsParam).getOrElse("")
+		val htmlData: NodeSeq = base.getParamOption(auxParam).getOrElse(Some(Nil)).getOrElse(Nil)
+		val choices = if (base.getParamOption(shuffleChoicesParam).getOrElse(true)) Random.shuffle(input) else input
 
-				MultipleChoiceQuery(instructions, choices.map(_.value), maxAnswers, minAnswers, ih.instructionTitle + " [" + Math.abs(Random.nextDouble()) + "]")
-			case _ => throw new IllegalStateException("Default MC Query is only supported for Instruction-Handling enabled processes")
-		}
+		val (instructions, instructionTitle) = prepareInstructions(base, nonBaseClassInstructionGenerator)
+
+		MultipleChoiceQuery(instructions.getInstructions(instructionItalic, htmlData = htmlData), choices.map(_.value), maxAnswers, minAnswers, instructionTitle)
 	}
 
 	override def parseAnswer[TARGET](input: List[Patch], answer: HCompAnswer, base: ProcessStub[_, _])(implicit baseCls: ClassTag[TARGET]): Option[TARGET] = {
@@ -39,6 +53,9 @@ class DefaultMCQueryBuilder(maxAnswers: Int = 1, minAnswers: Int = 1, italicInst
 			case s: Class[String] =>
 				assert(maxAnswers == 1)
 				Some(castedAnswer.selectedAnswer)
+			case s: Class[Patch] =>
+				assert(maxAnswers == 1)
+				Some(input.find(i => i.value == castedAnswer.selectedAnswer).get)
 			case li: Class[List[String]] =>
 				Some(castedAnswer.selectedAnswers)
 			case _ => None
@@ -50,20 +67,40 @@ class DefaultMCQueryBuilder(maxAnswers: Int = 1, minAnswers: Int = 1, italicInst
 class DefaultTextQueryBuilder(italicInstructionsParam: ProcessParameter[String] = INSTRUCTIONS_ITALIC,
 							  auxParam: ProcessParameter[Option[NodeSeq]] = QUESTION_AUX) extends HCompQueryBuilder[Patch] {
 
-	override def buildQuery(input: Patch, base: ProcessStub[_, _], payload: Any): HCompQuery = base match {
-		case ih: InstructionHandler =>
-			val instructionItalic: String = base.getParamOption(italicInstructionsParam).getOrElse("")
-			val htmlData: NodeSeq = base.getParamOption(auxParam).getOrElse(Some(Nil)).getOrElse(Nil)
-			val instructions: String = ih.instructions.getInstructions(instructionItalic, htmlData = htmlData)
+	override def buildQuery(input: Patch, base: ProcessStub[_, _], nonBaseClassInstructionGenerator: Option[InstructionGenerator]): HCompQuery = {
+		val instructionItalic: String = base.getParamOption(italicInstructionsParam).getOrElse("")
+		val htmlData: NodeSeq = base.getParamOption(auxParam).getOrElse(Some(Nil)).getOrElse(Nil)
 
-			FreetextQuery(instructions, "", ih.instructionTitle + " [" + Math.abs(Random.nextDouble()) + "]")
-		case _ => throw new IllegalStateException("Default Text Query is only supported for Instruction-Handling enabled processes")
+		val (instructions, instructionTitle) = prepareInstructions(base, nonBaseClassInstructionGenerator)
+
+		FreetextQuery(instructions.getInstructions(instructionItalic, htmlData = htmlData), "", instructionTitle)
 	}
 
 	override def parseAnswer[TARGET](input: Patch, answer: HCompAnswer, base: ProcessStub[_, _])(implicit baseCls: ClassTag[TARGET]): Option[TARGET] = {
 		Some(answer.is[FreetextAnswer].answer).asInstanceOf[Option[TARGET]]
 	}
 }
+
+
+class DefaultDoubleQueryBuilder(italicInstructionsParam: ProcessParameter[String] = INSTRUCTIONS_ITALIC,
+								auxParam: ProcessParameter[Option[NodeSeq]] = QUESTION_AUX) extends HCompQueryBuilder[Patch] {
+
+	override def buildQuery(input: Patch, base: ProcessStub[_, _], nonBaseClassInstructionGenerator: Option[InstructionGenerator]): HCompQuery = {
+		val instructionItalic: String = base.getParamOption(italicInstructionsParam).getOrElse(" Please enter a percentage followed by the '%' sign. For example: 59%. Please use only integer percentages (e.g. 23.5% is not valid, 23% is). Your number should be between 0% and 100%. Your estimates for all of these questions must sum up to 100%.")
+		val htmlData: NodeSeq = base.getParamOption(auxParam).getOrElse(Some(Nil)).getOrElse(Nil)
+
+		val (instructions, instructionTitle) = prepareInstructions(base, nonBaseClassInstructionGenerator)
+
+		FreetextQuery(instructions.getInstructions(instructionItalic, htmlData = htmlData), "", instructionTitle)
+	}
+
+	override def parseAnswer[TARGET](input: Patch, answer: HCompAnswer, base: ProcessStub[_, _])(implicit baseCls: ClassTag[TARGET]): Option[TARGET] = {
+		val textAnswer = answer.is[FreetextAnswer].answer
+		val doubleAnswer = textAnswer.replaceAll("[^0-9]", "").toDouble
+		Some(doubleAnswer).asInstanceOf[Option[TARGET]]
+	}
+}
+
 
 trait HCompQueryBuilderSupport[INPUT] extends IParametrizable {
 	self: ProcessStub[INPUT, _] =>
--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/BayesianTruthContest.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/process/stdlib/BayesianTruthContest.scala
@@ -24,25 +24,48 @@ class BayesianTruthContest(params: Map[String, Any] = Map.empty[String, Any]) ex
 				memoizer.mem("bayesianTruth" + w)(
 					U.retry(2) {
 						val ownOpinion = createMCQueryForOwnOpinion(alternatives)
-
+						val opinionsOnOtherPatches = alternatives.map(p => createTextFieldForOthersOpinions(p))
 						portal.sendQueryAndAwaitResult(
-							createComposite(ownOpinion),
+							createComposite(ownOpinion :: opinionsOnOtherPatches),
 							QUESTION_PRICE.get
 						) match {
-							case Some(a: CompositeQueryAnswer) => (a, a.get[HCompAnswer](ownOpinion))
-							case _ => {
+							case Some(a: CompositeQueryAnswer) =>
+								val rawOwnAnswer: HCompAnswer = a.get[HCompAnswer](ownOpinion)
+								val parsedOwnAnswer: Patch = queryBuilder.parseAnswer[Patch](alternatives, rawOwnAnswer, this).get
+
+								val rawAnswersForOtherPatches = opinionsOnOtherPatches.zipWithIndex.map(pi => alternatives(pi._2) -> a.get[HCompAnswer](pi._1)).toMap
+								val parsedAnswersForOtherPatches = rawAnswersForOtherPatches.map(r => r._1 -> otherOpinionsQueryBuilder.parseAnswer[Double](r._1, r._2, this).get)
+								BTAnswer(a, parsedOwnAnswer, parsedAnswersForOtherPatches)
+							case _ =>
 								logger.info(s"${getClass.getSimpleName} didn't get answer for query. retrying..")
 								throw new IllegalStateException("didnt get any response")
-							}
+
 						}
 					}
 				)).toList
 
-			val valueOfAnswer: Option[String] = answers.groupBy(s => queryBuilder.parseAnswer[String](alternatives, s._2, this)).maxBy(s => s._2.length)._1
-			logger.info("got answer " + valueOfAnswer)
-			val p = alternatives.find(_.value == valueOfAnswer.get).get
-			addInjectedAnswersToPatch(p, answers.map(_._1))
-			p
+			val prunedAnswers = answers.filter(a => {
+				val totalPercentage: Double = a.probabilitiesForOtherAnswers.values.sum
+				totalPercentage > 99 && totalPercentage < 101
+			})
+
+			val avgOwnAnswers = alternatives.map(a => a -> (prunedAnswers.count(p => p.ownAnswer == a).toDouble / prunedAnswers.length.toDouble)).toMap
+			val avgOthersAnswers = alternatives.map(a => a -> prunedAnswers.map(p => p.probabilitiesForOtherAnswers(a)).sum / 100d).toMap
+
+			val selectedAnswer = prunedAnswers.maxBy(_.btsScore(avgOwnAnswers, avgOthersAnswers)).ownAnswer
+			logger.info(s"selected answer $selectedAnswer")
+
+			addInjectedAnswersToPatch(selectedAnswer, prunedAnswers.map(_.rawAnswer))
+			selectedAnswer
+		}
+	}
+
+	private case class BTAnswer(rawAnswer: CompositeQueryAnswer, ownAnswer: Patch, probabilitiesForOtherAnswers: Map[Patch, Double]) {
+
+		def btsScore(avgOwn: Map[Patch, Double], avgOthers: Map[Patch, Double]) = {
+			val leftTerm = Math.log(avgOwn(ownAnswer) / avgOthers(ownAnswer))
+			val rightTerm = probabilitiesForOtherAnswers.map(p => avgOwn(p._1) * Math.log(p._2 / avgOwn(p._1))).sum
+			leftTerm + rightTerm
 		}
 	}
 
@@ -50,8 +73,10 @@ class BayesianTruthContest(params: Map[String, Any] = Map.empty[String, Any]) ex
 		queryBuilder.buildQuery(alternatives, this)
 	}
 
-	def createTextFieldForOthersOpinions(patch: Patch) = OTHERS_OPINIONS_QUERY_BUILDER.get.buildQuery(patch, this)
+	def createTextFieldForOthersOpinions(patch: Patch) = otherOpinionsQueryBuilder.buildQuery(patch, this)
 
+
+	protected def otherOpinionsQueryBuilder: HCompQueryBuilder[Patch] = OTHERS_OPINIONS_QUERY_BUILDER.get
 
 	override val processParameterDefaults: Map[ProcessParameter[_], List[Any]] = {
 		Map(queryBuilderParam -> List(new DefaultMCQueryBuilder()))
@@ -61,6 +86,8 @@ class BayesianTruthContest(params: Map[String, Any] = Map.empty[String, Any]) ex
 		List(WORKER_COUNT, BayesianTruthContest.OTHERS_OPINIONS_INSTRUCTION_GENERATOR) ::: super.optionalParameters
 
 	override def getCostCeiling(data: List[Patch]): Int = WORKER_COUNT.get * QUESTION_PRICE.get.paymentCents
+
+
 }
 
 object BayesianTruthContest {
