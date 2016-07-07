15b9113e34de075659c4d5cd65fd98456e98ca3c
Patrick de Boer <pdeboer@mit.edu>
2016-07-07 18:45:45 +0200

    experiment 3

---
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnGDP_Experiment3.scala
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.pdeboer.pplib.examples.gdpinfluence
+
+import ch.uzh.ifi.pdeboer.pplib.hcomp.dbportal.MySQLDBPortalDecorator
+import ch.uzh.ifi.pdeboer.pplib.hcomp.{FreetextQuery, HComp, HCompQueryProperties}
+import ch.uzh.ifi.pdeboer.pplib.process.entities.DefaultParameters._
+import ch.uzh.ifi.pdeboer.pplib.process.entities.{IndexedPatch, TrivialInstructionGenerator}
+import ch.uzh.ifi.pdeboer.pplib.process.stdlib.Contest
+import ch.uzh.ifi.pdeboer.pplib.util.{CollectionUtils, LazyLogger, U}
+import com.github.tototoshi.csv.CSVReader
+
+/**
+  * Created by pdeboer on 06/07/16.
+  */
+object FeatureInfluenceOnGDP_Experiment3 extends App with LazyLogger {
+	val features = CSVReader.open("example_data/featuresWithInfluenceOnGDP.csv").all().map(l => Feature(l.head)(l(2)))
+	//val features = List(Feature("f1")("have a higher or a lower share of their money made in the agricultural sector (meat/wheat production, farms..) .."), Feature("f2")("spend higher or lower amount of money of their government's budget on research .."))
+	val portal = HComp.mechanicalTurk
+	U.initDBConnection()
+
+	def getEstimationForFeature(feature: Feature) = {
+		val instructions = new TrivialInstructionGenerator("Please estimate the difference of..",
+			"Please estimate the influence of this economic factor", questionAfter = ".. between high-income countries and low-income countries. Example: countries with high average income tend to have much (6) lower crime rates than countries with lower average income. Feel free to take multiple of these HITS, but only answer each influencing factor once. ")
+		val contest = new Contest(Map(PORTAL_PARAMETER.key -> new MySQLDBPortalDecorator(portal, None),
+			WORKER_COUNT.key -> 31, OVERRIDE_INSTRUCTION_GENERATOR.key -> Some(instructions),
+			QUESTION_PRICE.key -> HCompQueryProperties(paymentCents = 4),
+			INJECT_QUERIES.key -> Map("why" -> FreetextQuery("why do you think so?")),
+			INSTRUCTIONS_ITALIC.key -> feature.description))
+		val choices: List[String] = List("1 [(almost?) no difference]", "2 [little difference]", "3 [noticeable difference]", "4 [significant difference]", "5 [very large difference]")
+		val res = contest.process(IndexedPatch.from(choices))
+		val votes = contest.extractContestResultFromWinner(res)
+		logger.info(s"Feature $feature received vote $votes")
+		votes
+	}
+
+	import CollectionUtils._
+
+	features.mpar.foreach(getEstimationForFeature)
+}
