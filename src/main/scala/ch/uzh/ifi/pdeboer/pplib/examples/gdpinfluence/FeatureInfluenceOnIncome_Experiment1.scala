b348ba7f5aa88bc597c469777e819deb96361507
Patrick de Boer <pdeboer@mit.edu>
2016-08-03 16:56:59 +0200

    more variables

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnIncome_Experiment1.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnIncome_Experiment1.scala
@@ -12,14 +12,14 @@ import com.github.tototoshi.csv.CSVReader
   * Created by pdeboer on 06/07/16.
   */
 object FeatureInfluenceOnIncome_Experiment1 extends App with LazyLogger {
-	val features = CSVReader.open("example_data/featuresWithInfluenceOnIncome.csv").all().map(l => Feature(l.head)(l(1)))
+	val features = CSVReader.open("example_data/featuresWithInfluenceOnIncome2.csv").all().map(l => Feature(l.head)(l(1)))
 	//val features = List(Feature("f1")("have a higher or a lower share of their money made in the agricultural sector (meat/wheat production, farms..) .."), Feature("f2")("spend higher or lower amount of money of their government's budget on research .."))
 	val portal = HComp.mechanicalTurk
 	U.initDBConnection()
 
 	def getEstimationForFeature(feature: Feature) = {
 		val instructions = new TrivialInstructionGenerator("What do you think about the following: In the USA, ",
-			"Please estimate the influence of this economic factor", questionAfter = "For example, people who earn more than $50k/yr tend to a lower amount of crimes recorded than people who earn less than $50k/yr. You can accept multiple of these HITs, but please only one per variable")
+			"Please estimate the influence of this economic factor", questionAfter = "You can accept multiple of these HITs, but please only *one per variable* (marked with the asterisks **)")
 		val contest = new Contest(Map(PORTAL_PARAMETER.key -> new MySQLDBPortalDecorator(portal, None),
 			WORKER_COUNT.key -> 35, OVERRIDE_INSTRUCTION_GENERATOR.key -> Some(instructions),
 			QUESTION_PRICE.key -> HCompQueryProperties(paymentCents = 5),
