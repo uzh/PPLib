d8c24fcdaf2051397fd55a7ef27ee9f472b3cdd6
Patrick de Boer <pdeboer@mit.edu>
2016-11-03 13:12:46 +0100

    more workers

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnOlympia_Experiment2.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnOlympia_Experiment2.scala
@@ -26,7 +26,7 @@ object FeatureInfluenceOnOlympia_Experiment2 extends App with LazyLogger {
 			"How well can you predict the olympics?", questionAfter = "Please only accept one of these HITs per day") //You can accept multiple of these HITs, but please only *one per topic* (topics marked with the asterisks **).
 		val choices: List[String] = (1 to 10).map(x => s"$x (${x}0%)").toList
 		val contest = new Contest(Map(PORTAL_PARAMETER.key -> new MySQLDBPortalDecorator(portal, None),
-			WORKER_COUNT.key -> 1, OVERRIDE_INSTRUCTION_GENERATOR.key -> Some(instructions),
+			WORKER_COUNT.key -> 20, OVERRIDE_INSTRUCTION_GENERATOR.key -> Some(instructions),
 			INSTRUCTIONS_ITALIC.key -> features.head.description,
 			INJECT_QUERIES.key -> features.drop(1).map(f => f.name -> MultipleChoiceQuery(f.description, Random.shuffle(choices), 1)).toMap
 		))
