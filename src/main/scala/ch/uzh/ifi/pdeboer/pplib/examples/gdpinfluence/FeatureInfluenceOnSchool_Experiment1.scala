a859cd9a981eff37f39fcba217caaa8e85c0c74c
Patrick de Boer <pdeboer@mit.edu>
2016-12-21 13:57:22 +0100

    marcel

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
@@ -1,11 +1,8 @@
 package ch.uzh.ifi.pdeboer.pplib.examples.gdpinfluence
 
 import ch.uzh.ifi.pdeboer.pplib.hcomp.dbportal.MySQLDBPortalDecorator
-import ch.uzh.ifi.pdeboer.pplib.hcomp.{HComp, MultipleChoiceQuery, RejectMultiAnswerHCompPortal}
-import ch.uzh.ifi.pdeboer.pplib.process.entities.{IndexedPatch, TrivialInstructionGenerator}
-import ch.uzh.ifi.pdeboer.pplib.process.stdlib.Contest
+import ch.uzh.ifi.pdeboer.pplib.hcomp.{FreetextQuery, HComp}
 import ch.uzh.ifi.pdeboer.pplib.util.{LazyLogger, U}
-import com.github.tototoshi.csv.CSVReader
 
 import scala.util.Random
 
@@ -13,20 +10,24 @@ import scala.util.Random
   * Created by pdeboer on 06/07/16.
   */
 object FeatureInfluenceOnSchool_Experiment1 extends App with LazyLogger {
-/*	val portal = HComp.mechanicalTurk
-	portal.approveAll = false
+	val portal = new MySQLDBPortalDecorator(HComp.mechanicalTurk)
+  HComp.mechanicalTurk.approveAll = false
 	U.initDBConnection()
 
-	def runSingleTask(): Unit = {
+	def runSingleTask(theNumber:Int) = {
 		val randomString = Random.alphanumeric.take(10).mkString
 		val theUrl = s"http://mbuehler.ch/?name=$randomString"
 
-		//FreetextQuery("test")
-		portal.sendQueryAndAwaitResult()
+
+		val answer = portal.sendQueryAndAwaitResult(FreetextQuery("test")).get
+
+    HComp.mechanicalTurk.rejectAnswer(answer, "asdf") //for rejection
+    HComp.mechanicalTurk.approveAndBonusAnswer(answer, "bla") //for approval
+
+    answer
 	}
 
-	import ch.uzh.ifi.pdeboer.pplib.util.CollectionUtils._
-
-	featureGroups.mpar.foreach(getEstimationForFeatureGroup)
-	println("done") */
+  import ch.uzh.ifi.pdeboer.pplib.util.CollectionUtils._ //for mpar
+  val myListWithAnswers = (1 to 10).mpar.map(runSingleTask)
+  print(myListWithAnswers)
 }
