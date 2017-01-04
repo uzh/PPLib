597802b97bfd0b714a9ac9e049177c265244c08b
Marcello <marcel.buehler@uzh.ch>
2017-01-04 20:08:45 +0100

    payment: 10 cents + further instructions

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
@@ -2,7 +2,7 @@ package ch.uzh.ifi.pdeboer.pplib.examples.gdpinfluence
 
 import ch.uzh.ifi.pdeboer.pplib.examples.gdpinfluence.HashValidation.validateToken
 import ch.uzh.ifi.pdeboer.pplib.hcomp.dbportal.MySQLDBPortalDecorator
-import ch.uzh.ifi.pdeboer.pplib.hcomp.{FreetextQuery, HComp}
+import ch.uzh.ifi.pdeboer.pplib.hcomp.{FreetextQuery, HComp, HCompQueryProperties}
 import ch.uzh.ifi.pdeboer.pplib.util.{LazyLogger, U}
 
 import scala.util.Random
@@ -12,24 +12,25 @@ import scala.util.Random
   */
 object FeatureInfluenceOnSchool_Experiment1 extends App with LazyLogger {
   val answerCount = 5
+  val paymentCents = 10
+  val dataset_name = s"student"// Adjust dataset name for other datasets
+
 	val portal = new MySQLDBPortalDecorator(HComp.mechanicalTurk)
   HComp.mechanicalTurk.approveAll = false
 
 	U.initDBConnection()
 
 	def runSingleTask(theNumber:Int) = {
-    // Adjust dataset name for other datasets
-    val dataset_name = s"student"
-
     val title = s"Ranking Survey"
     val condition = 1 // layperson
 		val randomString = Random.alphanumeric.take(10).mkString
 		val theUrl = s"""http://mbuehler.ch/?dataset_name=$dataset_name&amp;condition=$condition&amp;name=$randomString"""
-    val queryInstructions = s"""Please go to the url bellow or <a href="$theUrl" target="_blank">click here</a> and solve the given task. After submitting your answer, you will be given a token. Copy the token and paste it here to receive your reward.<br/>$theUrl"""
+    val queryInstructions = s"""Please go to the url bellow or <a href="$theUrl" target="_blank">click here</a> and solve the given task. After submitting your answer, you will be given a token. Copy the token and paste it here to receive your reward. You are only allowed to complete one of these HITs ("$title") per day.<br/>$theUrl"""
     val rejectedMsg = s"You submitted an invalid token. Please solve the given task and submit the full token displayed after solving the task."
     val approvedMsg = s"Successfully submitted your answer. Thank you for your efforts."
 
-		val answer = portal.sendQueryAndAwaitResult(FreetextQuery(queryInstructions, "", title)).get
+    val properties = HCompQueryProperties(paymentCents = paymentCents)
+		val answer = portal.sendQueryAndAwaitResult(FreetextQuery(queryInstructions, "", title), properties = properties).get
     val token = answer.toString() // answer is FreetextQueryAnswer, but we need String for comparison
 
     val isValid = validateToken(token, randomString)
