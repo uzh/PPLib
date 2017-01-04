b5cf11ba0d68ddf4ec4cd3fcb64039a836ba6dc9
Marcello <marcel.buehler@uzh.ch>
2017-01-04 15:47:08 +0100

    ready for student evaluation

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/examples/gdpinfluence/FeatureInfluenceOnSchool_Experiment1.scala
@@ -11,30 +11,37 @@ import scala.util.Random
   * Created by pdeboer on 06/07/16.
   */
 object FeatureInfluenceOnSchool_Experiment1 extends App with LazyLogger {
+  val answerCount = 5
 	val portal = new MySQLDBPortalDecorator(HComp.mechanicalTurk)
   HComp.mechanicalTurk.approveAll = false
+
 	U.initDBConnection()
 
 	def runSingleTask(theNumber:Int) = {
+    // Adjust dataset name for other datasets
+    val dataset_name = s"student"
+
+    val title = s"Ranking Survey"
+    val condition = 1 // layperson
 		val randomString = Random.alphanumeric.take(10).mkString
-		val theUrl = s"http://http://mbuehler.ch/?dataset_name=student&condition=1&name=$randomString"
-    val queryInstructions = s"Please go to the url bellow and solve the given task. After submitting your answer, you will be given a token. Copy the token and paste it here to receive your reward. URL: $theUrl"
+		val theUrl = s"""http://mbuehler.ch/?dataset_name=$dataset_name&amp;condition=$condition&amp;name=$randomString"""
+    val queryInstructions = s"""Please go to the url bellow or <a href="$theUrl" target="_blank">click here</a> and solve the given task. After submitting your answer, you will be given a token. Copy the token and paste it here to receive your reward.<br/>$theUrl"""
     val rejectedMsg = s"You submitted an invalid token. Please solve the given task and submit the full token displayed after solving the task."
     val approvedMsg = s"Successfully submitted your answer. Thank you for your efforts."
 
-		val answer = portal.sendQueryAndAwaitResult(FreetextQuery(queryInstructions)).get
+		val answer = portal.sendQueryAndAwaitResult(FreetextQuery(queryInstructions, "", title)).get
+    val token = answer.toString() // answer is FreetextQueryAnswer, but we need String for comparison
 
-    val token = "" // get answer token from answer object
     val isValid = validateToken(token, randomString)
+    println(s"is valid: $isValid")
     if (isValid) {
       HComp.mechanicalTurk.approveAndBonusAnswer(answer, approvedMsg) //for approval
-      println(answer)
     } else{
       HComp.mechanicalTurk.rejectAnswer(answer, rejectedMsg) //for rejection
     }
 	}
 
   import ch.uzh.ifi.pdeboer.pplib.util.CollectionUtils._ //for mpar
-  val myListWithAnswers = (1 to 10).mpar.map(runSingleTask)
+  val myListWithAnswers = (1 to answerCount).mpar.map(runSingleTask)
   print(myListWithAnswers)
 }
